d9f47d6d699a7e83ac03bc9012d3e7acbecf7b98
Elton Dakaj <elton235@gmail.com>
2016-01-14 00:04:22 -0500

    Initial commit

---
+++ app/src/main/java/com/elton/geoquiz/Question.java
@@ -0,0 +1,30 @@
+package com.elton.geoquiz;
+
+/**
+ * Created by elton on 1/12/16.
+ */
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
